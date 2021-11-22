1bbb478a1a97936d84712db8b6c995e8a7f1109e
Nate <nate@qstreet.org>
2021-11-21 19:18:07 -0500

    install nvm and setup with fish

--- setup.sh
+++ setup.sh
@@ -1,35 +1,28 @@
 #! /bin/bash
-
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 1000
 defaults write com.apple.dock no-bouncing -bool TRUE
 defaults write com.apple.dock persistent-apps "()"
 defaults write com.apple.dock show-recents 0
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-chflags hidden /Users/nate/Applications /Users/nate/Desktop /Users/nate/Documents /Users/nate/Movies /Users/nate/Music /Users/nate/Pictures /Users/nate/Public 
+
+chflags hidden ~/Applications ~/Desktop ~/Documents ~/Movies ~/Music ~/Pictures ~/Public 
 
 mkdir ~/Code ~/School
 touch ~/.hushlogin
+touch ~/Library/Application\ Support/Code/User/settings.json
+touch ~/.config/fish/functions/nvm.fish
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew update
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
 brew install --cask google-chrome
-brew install --cask visual-studio-code 
 brew install --cask homebrew/cask-drivers/steelseries-exactmouse-tool
 brew install --cask tor-browser
-brew install fish
 
-echo "/usr/local/bin/fish
-/bin/bash
-/bin/zsh" | sudo tee /etc/shells
-
-chsh -s /usr/local/bin/fish
-
-fish -c "set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths && set -U fish_greeting && set -U fish_greeting"
-
-touch ~/Library/Application\ Support/Code/User/settings.json
+brew install --cask visual-studio-code 
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 echo `{
   "workbench.colorTheme": "One Dark Pro",
   "workbench.iconTheme": "material-icon-theme",
@@ -40,19 +33,28 @@ echo `{
   "editor.minimap.enabled": false,
   "workbench.startupEditor": "none",
   "explorer.confirmDelete": false,
-  // "terminal.integrated.defaultProfile.osx": "bash",
   "terminal.integrated.defaultProfile.osx": "fish",
-  "[rust]": {
-    "editor.defaultFormatter": "matklad.rust-analyzer"
-  },
+  // "terminal.integrated.defaultProfile.osx": "bash",
   "files.exclude": {
     "**/node_modules": true,
     "**/package-lock.json": true,
     "**/yarn.lock": true
   }
-}` | sudo tee ~/Library/Application\ Support/Code/User/settings.json
-
+}` | tee ~/Library/Application\ Support/Code/User/settings.json
 for extension in esbenp.prettier-vscode PKief.material-icon-theme ritwickdey.LiveServer vscodevim.vim zhuangtongfa.material-theme
   do code --install-extension $extension
 done
 
+brew install fish
+echo "/usr/local/bin/fish
+/bin/bash
+/bin/zsh" | tee /etc/shells
+chsh -s /usr/local/bin/fish
+fish -c "set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths && set -U fish_greeting && set -U fish_greeting"
+
+fisher add edc/bass
+
+echo "function nvm
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end" | tee ~/.config/fish/functions/nvm.fish
+nvm install node
