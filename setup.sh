a00e9f6aa62fd120f8eae16e57a5d1853c54b820
NaeNate <37544626+NaeNate@users.noreply.github.com>
2021-12-11 00:20:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,6 @@ defaults write com.apple.dock show-recents 0
 mkdir ~/Code ~/School
 sudo touch ~/.hushlogin
 
-sudo rm -rf ~/Movies ~/Music ~/Public ~/Pictures ~/Documents
-
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -19,23 +17,26 @@ brew install --cask google-chrome
 
 brew install --cask visual-studio-code 
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-# echo "{
-#   \"workbench.colorTheme\": \"One Dark Pro\",
-#   \"workbench.iconTheme\": \"material-icon-theme\",
-#   \"editor.defaultFormatter\": \"esbenp.prettier-vscode\",
-#   \"editor.formatOnSave\": true,
-#   \"editor.tabSize\": 4,
-#   \"editor.minimap.enabled\": false,
-#   \"workbench.startupEditor\": \"none\",
-#   \"explorer.confirmDelete\": false,
-#   \"terminal.integrated.defaultProfile.osx\": \"fish\",
-#   // \"terminal.integrated.defaultProfile.osx\": \"bash\",
-#   \"files.exclude\": {
-#     \"**/node_modules\": true,
-#     \"**/package-lock.json\": true,
-#     \"**/yarn.lock\": true
-#   }
-# }" | tee "~/Library/Application\ Support/Code/User/settings.json"
+mkdir -p "/Users/nate/Library/Application\ Support/Code/User" && touch "~/Library/Application\ Support/Code/User/settings.json"
+ echo "{
+   \"workbench.colorTheme\": \"One Dark Pro\",
+   \"workbench.iconTheme\": \"material-icon-theme\",
+   \"editor.defaultFormatter\": \"esbenp.prettier-vscode\",
+   \"editor.formatOnSave\": true,
+   \"editor.minimap.enabled\": false,
+   \"editor.tabSize\": 4,
+   \"window.zoomLevel\": 1,
+   \"workbench.startupEditor\": \"none\",
+   \"explorer.confirmDelete\": false,
+   \"terminal.integrated.defaultProfile.osx\": \"fish\",
+   // \"terminal.integrated.defaultProfile.osx\": \"bash\",
+   
+   \"files.exclude\": {
+     \"**/node_modules\": true,
+     \"**/package-lock.json\": true,
+     \"**/yarn.lock\": true
+   }
+ }" | tee "~/Library/Application\ Support/Code/User/settings.json"
 
 for extension in esbenp.prettier-vscode PKief.material-icon-theme ritwickdey.LiveServer vscodevim.vim zhuangtongfa.material-theme
   do code --install-extension $extension
@@ -46,12 +47,18 @@ echo "/usr/local/bin/fish
 /bin/bash
 /bin/zsh" | sudo tee /etc/shells
 chsh -s /usr/local/bin/fish
-fish -c "set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths && set -U fish_greeting && fish_vi_key_bindings"
+fish -c "set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths" 
+fish -c "set -U fish_greeting"
+fish -c "fish_vi_key_bindings"
 
-# echo "function fish_prompt
-#     echo -n -s (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) ">"
-# end" | tee "./config/fish/functions/fish_prompt.fish"
+touch "~/.config/fish/functions/fish_prompt.fish"
+
+ echo "function fish_prompt
+     echo -n -s (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) ">"
+ end" | tee "./config/fish/functions/fish_prompt.fish"
 
 brew install node
 
 brew install git
+
+sudo rm -rf ~/Movies ~/Music ~/Public ~/Pictures ~/Documents
